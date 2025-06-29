6215045d3092e4caba9524781e22b6733c6f88d6
Ale-fm03 <142700930+Ale-fm03@users.noreply.github.com>
2025-06-29 13:04:07 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,12 @@
 <?php
+
+header("Content-Security-Policy: default-src 'self';");
+header("X-Frame-Options: DENY");
+header("X-XSS-Protection: 1; mode=block");
+header("Strict-Transport-Security: max-age=31536000; includeSubDomains");
+header("X-Content-Type-Options: nosniff");
+header_remove("Server");
+
     require_once "models/DataBase.php";   
     if (!isset($_REQUEST['c'])) {
         require_once "controllers/Usuarios.php";
